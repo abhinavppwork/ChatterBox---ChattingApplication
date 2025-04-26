489c2abfa1df0502ddb42d335b5014bfab25fb95
Ramnagendra2005 <sripadaram2005@gmail.com>
2025-04-27 02:05:44 +0530

    message

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,35 +1,36 @@
 import express from "express";
-import dotenv from "dotenv";
-import authRoutes from './routes/auth.route.js';
+import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
-import messageRoutes from './routes/message.route.js';
+import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
+import dotenv from "dotenv";
 import path from "path";
-import { app,server } from "./lib/socket.js"
-dotenv.config();
 
-
+dotenv.config({ path: path.resolve("../.env") });
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: '50mb' })); 
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
-}));
+  }),
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-if (process.env.NODE_ENV ==="production") {
-    app.use(express.static(path.join(__dirname, "../frontend/build")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));  
-    });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 server.listen(PORT, () => {
-    connectDB();
-    console.log("server is running on PORT: " + PORT);
+  connectDB();
+  console.log("server is running on PORT: " + PORT);
 });
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
-export const connectDB = async() =>{
-    try{
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`mongodb connected:${conn.connection.host}`)
-    }catch(err) {
-        console.log("error occured",err.message)
-    }
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.log("error occurred", err.message);
+  }
 };
